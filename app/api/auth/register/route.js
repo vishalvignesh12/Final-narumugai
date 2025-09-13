cbef39da7cccae566ec7c2c317b4cdae82591931
vishalvignesh12 <vishalvignesh12@gmail.com>
2025-09-13 13:19:15 +0530

    First commit

---
+++ app/api/auth/register/route.js
@@ -0,0 +1,56 @@
+import { emailVerificationLink } from "@/email/emailVerificationLink";
+import { connectDB } from "@/lib/databaseConnection";
+import { catchError, response } from "@/lib/helperFunction";
+import { sendMail } from "@/lib/sendMail";
+import { zSchema } from "@/lib/zodSchema";
+import UserModel from "@/models/User.model";
+import { SignJWT } from "jose";
+
+export async function POST(request) {
+    try {
+        await connectDB()
+        // validation schema  
+        const validationSchema = zSchema.pick({
+            name: true, email: true, password: true
+        })
+
+        const payload = await request.json()
+
+        const validatedData = validationSchema.safeParse(payload)
+
+        if (!validatedData.success) {
+            return response(false, 401, 'Invalid or missing input field.', validatedData.error)
+        }
+
+        const { name, email, password } = validatedData.data
+
+        // check already registered user 
+        const checkUser = await UserModel.exists({ email })
+        if (checkUser) {
+            return response(true, 409, 'User already registered.')
+        }
+
+        // new registration  
+
+        const NewRegistration = new UserModel({
+            name, email, password
+        })
+
+        await NewRegistration.save()
+
+        const secret = new TextEncoder().encode(process.env.SECRET_KEY)
+        const token = await new SignJWT({ userId: NewRegistration._id.toString() })
+            .setIssuedAt()
+            .setExpirationTime('1h')
+            .setProtectedHeader({ alg: 'HS256' })
+            .sign(secret)
+
+
+        await sendMail('Email Verification request from Developer Goswami', email, emailVerificationLink(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/verify-email/${token}`))
+
+        return response(true, 200, 'Registration success, Please verify your email address.')
+
+    } catch (error) {
+        catchError(error)
+    }
+}
---
+++ components/Application/Website/Footer.jsx
@@ -0,0 +1,122 @@
+import Image from 'next/image'
+import React from 'react'
+import logo from '@/public/assets/images/logo-black.png'
+import Link from 'next/link'
+import { IoLocationOutline } from "react-icons/io5";
+import { MdOutlinePhone } from "react-icons/md";
+import { MdOutlineMail } from "react-icons/md";
+import { AiOutlineYoutube } from "react-icons/ai";
+import { FaInstagram } from "react-icons/fa";
+import { FaWhatsapp } from "react-icons/fa";
+import { TiSocialFacebookCircular } from "react-icons/ti";
+import { FiTwitter } from "react-icons/fi";
+
+import { USER_DASHBOARD, WEBSITE_HOME, WEBSITE_LOGIN, WEBSITE_REGISTER, WEBSITE_SHOP } from '@/routes/WebsiteRoute'
+const Footer = () => {
+    return (
+        <footer className='bg-gray-50 border-t'>
+            <div className='grid lg:grid-cols-5 md:grid-cols-2 grid-cols-1 gap-10 py-10 lg:px-32 px-4'>
+
+                <div className='lg:col-span-1 md:col-span-2 col-span-1'>
+                    <h1 className='text-4xl font-bold text-pink-500 mb-3'>Narumugai</h1>
+                    <p className='text-gray-500 text-sm'>
+                        E-store is your trusted destination for quality and convenience. From fashion to essentials, we bring everything you need right to your doorstep. Shop smart, live better — only at E-store.
+                    </p>
+                </div>
+
+                <div>
+                    <h4 className='text-xl font-bold uppercase mb-5'>Userfull Links</h4>
+                    <ul>
+                        <li className='mb-2 text-gray-500'>
+                            <Link href={WEBSITE_HOME}>Home</Link>
+                        </li>
+                        <li className='mb-2 text-gray-500'>
+                            <Link href={WEBSITE_SHOP}>Shop</Link>
+                        </li>
+                        <li className='mb-2 text-gray-500'>
+                            <Link href="/about-us">About</Link>
+                        </li>
+                        <li className='mb-2 text-gray-500'>
+                            <Link href={WEBSITE_REGISTER}>Register</Link>
+                        </li>
+                        <li className='mb-2 text-gray-500'>
+                            <Link href={WEBSITE_LOGIN}>Login</Link>
+                        </li>
+
+                    </ul>
+                </div>
+                <div>
+                    <h4 className='text-xl font-bold uppercase mb-5'>Help Center</h4>
+                    <ul>
+                        <li className='mb-2 text-gray-500'>
+                            <Link href={WEBSITE_REGISTER}>Register</Link>
+                        </li>
+                        <li className='mb-2 text-gray-500'>
+                            <Link href={WEBSITE_LOGIN}>Login</Link>
+                        </li>
+                        <li className='mb-2 text-gray-500'>
+                            <Link href={USER_DASHBOARD}>My Account</Link>
+                        </li>
+                        <li className='mb-2 text-gray-500'>
+                            <Link href="/privacy-policy">Privacy Policy</Link>
+                        </li>
+                        <li className='mb-2 text-gray-500'>
+                            <Link href="/terms-and-conditions">Terms & Conditions</Link>
+                        </li>
+
+
+                    </ul>
+                </div>
+                <div>
+                    <h4 className='text-xl font-bold uppercase mb-5'>Contact Us </h4>
+                    <ul>
+                        <li className='mb-2 text-gray-500 flex gap-2'>
+                            <IoLocationOutline size={20} />
+                            <span className='text-sm'>E-store market Lucknow, India 256320</span>
+                        </li>
+                        <li className='mb-2 text-gray-500 flex gap-2'>
+                            <MdOutlinePhone size={20} />
+                            <Link href="tel:+91-8569874589" className='hover:text-primary text-sm'>+91-8569874589</Link>
+                        </li>
+                        <li className='mb-2 text-gray-500 flex gap-2'>
+                            <MdOutlineMail size={20} />
+                            <Link href="mailto:support@estore.com" className='hover:text-primary text-sm'>support@estore.com</Link>
+                        </li>
+
+                    </ul>
+
+
+                    <div className='flex gap-5 mt-5'>
+
+                        <Link href="">
+                            <AiOutlineYoutube className='text-primary' size={25} />
+                        </Link>
+                        <Link href="">
+                            <FaInstagram className='text-primary' size={25} />
+                        </Link>
+                        <Link href="">
+                            <FaWhatsapp className='text-primary' size={25} />
+                        </Link>
+                        <Link href="">
+                            <TiSocialFacebookCircular className='text-primary' size={25} />
+                        </Link>
+                        <Link href="">
+                            <FiTwitter className='text-primary' size={25} />
+                        </Link>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+            <div className='py-5 bg-gray-100' >
+                <p className='text-center'>© 2024 Estore. All Rights Reserved.</p>
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer
